78ed0fc926d81b5327703365684867c4d9d8a0eb
Thibaut Cornil <thibaut.cornil@hotmail.fr>
2022-02-03 11:39:41 +0100

    init

--- src/containers/Site/Localisation/Etablissement/Horaire/Horaire.js
+++ src/containers/Site/Localisation/Etablissement/Horaire/Horaire.js
@@ -2,16 +2,26 @@ import React from "react";
 
 const Horaires = props => {
     let content = props.horaires.map((horaire, index) => { /* on met les valeurs de horaires.map pour parcourir le tableau, on récupère les horaires et les index */
-        return <li key={index}>Du {horaire.du} au {horaire.au} - De {horaire.heures[0].de} à {horaire.heures[0].a}</li>
-    }); 
-return(
-    <>
-        <ul>
-        Horaire :
-        {content}
-        </ul>
-    </>
-);
+        return <li key={index}>
+            {
+                horaire.du === horaire.au ? `${horaire.du}` : `Du ${horaire.du} au ${horaire.au}` //Condition si le jour et le meme que le prochain jour alors j'affiche que le premier jour
+            }
+
+            - De {horaire.heures[0].de} à {horaire.heures[0].a}
+            {
+                horaire.heures[1] &&
+                <> et de {horaire.heures[1].de} à {horaire.heures[1].a}</>
+            }
+        </li>
+    });
+    return (
+        <>
+            <ul>
+                Horaire :
+                {content}
+            </ul>
+        </>
+    );
 
 }
 export default Horaires;
